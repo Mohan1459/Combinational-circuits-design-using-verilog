6a71cc48f281ce150e269d18072ddc71d819fed8
Mohan1459 <157452661+Mohan1459@users.noreply.github.com>
2025-09-17 11:16:59 +0530

    Create 4_2_encoder.v
    The 4:2 encoder is implemented using the case statement in verilog

---
+++ ENCODERS/4:2 ENCODERS/4_2_encoder.v
@@ -0,0 +1,29 @@
+module enc_4_2(
+  input [3:0]i,
+  output reg [1:0]y
+);
+  always@(*)begin
+    y<=2'b00;
+    case(i)
+      4'b0001:begin
+        y[0]=1'b0;
+        y[1]=1'b0;
+      end
+      4'b0010:begin
+        y[0]=1'b1;
+        y[1]=1'b0;
+      end
+      4'b0100:begin
+        y[0]=1'b0;
+        y[1]=1'b1;
+      end
+      4'b1000:begin
+        y[0]=1'b1;
+        y[1]=1'b1;
+      end
+      default:begin
+        y=2'b00;
+      end
+    endcase
+  end
+endmodule
